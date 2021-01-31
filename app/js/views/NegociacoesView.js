20b22dda06d98a9b00a1256100a43531350d32ee
David Edson <david.contato.tec@gmail.com>
2021-01-31 14:47:22 -0300

    Adicionado system que e um loaded de scripts e incluido no index, colocado nas configs do package.json para fazer a compilaçao em tempo real

--- app/js/views/NegociacoesView.js
+++ app/js/views/NegociacoesView.js
@@ -1,7 +1,17 @@
-import { View } from './View';
-export class NegociacoesView extends View {
-    template(model) {
-        return `
+System.register(["./View"], function (exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var View_1, NegociacoesView;
+    return {
+        setters: [
+            function (View_1_1) {
+                View_1 = View_1_1;
+            }
+        ],
+        execute: function () {
+            NegociacoesView = class NegociacoesView extends View_1.View {
+                template(model) {
+                    return `
         <table class="table table-hover table-bordered">
             <thead>
                 <tr>
@@ -15,7 +25,7 @@ export class NegociacoesView extends View {
             <tbody>
 
                 ${model.listar().map(negociacao => {
-            return `
+                        return `
                         <tr>
                             <td>${negociacao.data.getDate()}/${negociacao.data.getMonth() + 1}/${negociacao.data.getFullYear()}</td>
                             <td>${negociacao.quantidade}</td>
@@ -23,7 +33,7 @@ export class NegociacoesView extends View {
                             <td>${negociacao.volume}</td>
                         </tr>
                     `;
-        }).join('')}
+                    }).join('')}
 
             </tbody>
             
@@ -31,5 +41,9 @@ export class NegociacoesView extends View {
             </tfoot>
         </table>
         `;
-    }
-}
+                }
+            };
+            exports_1("NegociacoesView", NegociacoesView);
+        }
+    };
+});
